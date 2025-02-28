1d19da5a5f406824e90d67a0f01d5f13e47a55fe
Haiwen Diao <37319709+Paranioar@users.noreply.github.com>
2025-02-28 18:18:13 +0800

    Update vision_tokenizer.py

--- EVEv2/eve/model/multimodal_encoder/vision_tokenizer.py
+++ EVEv2/eve/model/multimodal_encoder/vision_tokenizer.py
@@ -48,7 +48,7 @@ class VisionTokenizer(nn.Module):
                                              nn.Conv2d(input_size, output_size, 
                                                        kernel_size=self.dense_stride, 
                                                        stride=self.dense_stride))
-        self.start_embedding = nn.Parameter(torch.randn(output_size))
+        self.class_embedding = nn.Parameter(torch.randn(output_size))
         self.split_embedding = nn.Parameter(torch.randn(output_size))
 
     def forward(self, pixel_values):
@@ -60,7 +60,7 @@ class VisionTokenizer(nn.Module):
             split_embed = self.split_embedding[:, None, None].repeat(1, patch_embed.shape[1], 1)
             patch_embed = torch.cat([patch_embed, split_embed.to(dtype=self.dtype)], dim=-1)
 
-            class_embed = self.start_embedding[None, :].to(dtype=self.dtype)
+            class_embed = self.class_embedding[None, :].to(dtype=self.dtype)
             patch_embeds.append(torch.cat([class_embed, patch_embed.flatten(1).transpose(0, 1)], dim=0))
 
         return patch_embeds
